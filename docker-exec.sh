50de6553cac0ecbdbc0725e28ad9a4fa5383b57b
TianyiChen0316 <tianyichen@stu.pku.edu.cn>
2024-12-29 12:24:05 +0800

    Add default arguments to docker-exec.sh

--- docker-exec.sh
+++ docker-exec.sh
@@ -2,4 +2,8 @@
 curdir=$(cd `dirname $0`; pwd)
 cd $curdir
 coordinator=$(cat "${curdir}/container-configs/hosts" | sed -n 1,1p | awk '{print $2}')
-docker exec -it gpdb-${coordinator} $@
+if [ -z "$*" ] ; then
+  docker exec -it gpdb-${coordinator} /bin/bash
+else
+  docker exec -it gpdb-${coordinator} $@
+fi
